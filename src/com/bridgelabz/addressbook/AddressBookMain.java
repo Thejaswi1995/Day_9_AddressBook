42d1d31e56fc56749cb397f8e96d30acf53c319b
Thejaswi <thejaswirl187@gmail.com>
2022-05-06 12:43:20 +0530

    Added : Address Book

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -5,7 +5,6 @@ package com.bridgelabz.addressbook;
         public static void main(String[] args) {
             System.out.println("Welcome to the Address Book Program");
             AddressBook contact = new AddressBook();
-            contact.setUpInfo();
             contact.displayListItems();
 
         }
