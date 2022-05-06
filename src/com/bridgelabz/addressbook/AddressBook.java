1901309e671c4dc657f88b9b2dac8409baf7737d
Thejaswi <thejaswirl187@gmail.com>
2022-05-06 12:52:32 +0530

    Added : Edit existing contact no

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
     public class AddressBook {
         List<Address> list = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
 
         public void setUpInfo() {
             Scanner in = new Scanner(System.in);
             System.out.println("How many contacts you want to create");
-            int contactcount = in.nextInt();
-            for (int i = 1; i <= contactcount; i++) {
+            int count = in.nextInt();
+            for (int i = 1; i <= count; i++) {
                 System.out.println("Enter the First Name:");
                 String firstname = in.next();
                 System.out.println("Enter the Last Name:");
@@ -39,6 +40,38 @@ import java.util.Scanner;
                         + "  " + info.getPincode() + "  " + info.getPhoneNo() + "  " + info.getEmail());
             }
 
+        }
+
+
+        public void editContact() {
+            System.out.println("Enter the first name of the contact to be edited");
+            String newName = in.next();
+            for (Address info : list) {
+                if (info.getFirstName().contains(newName)) {
+                    int index = list.indexOf(info);
+                    System.out.println(info.getFirstName() + "  " + info.getLastName() + "  "
+                            + info.getAddress() + "  " + info.getCity() + "  " + info.getState()
+                            + "  " + info.getPincode() + "  " + info.getPhoneNo() + "  " + info.getEmail());
+                    System.out.println("Enter the First Name:");
+                    String firstname = in.next();
+                    System.out.println("Enter the Last Name:");
+                    String lastname = in.next();
+                    System.out.println("Enter the Address in words:");
+                    String address = in.next();
+                    System.out.println("Enter the City:");
+                    String city = in.next();
+                    System.out.println("Enter the State Name:");
+                    String state = in.next();
+                    System.out.println("Enter the Pincode code:");
+                    int pincode = in.nextInt();
+                    System.out.println("Enter the Phone No");
+                    long phone = in.nextLong();
+                    System.out.println("Enter email id:");
+                    String email = in.next();
+                    list.set(index, new Address(firstname, lastname, address, city, state, pincode, phone, email));
+                    displayListItems();
+                }
+            }
         }
     }
 
--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,11 +1,33 @@
 package com.bridgelabz.addressbook;
-
+import java.util.Scanner;
     public class AddressBookMain
     {
         public static void main(String[] args) {
             System.out.println("Welcome to the Address Book Program");
+            AddressBookMain main = new AddressBookMain();
+            main.operation();
+        }
+
+        private void operation(){
             AddressBook contact = new AddressBook();
+            contact.setUpInfo();
             contact.displayListItems();
 
+            int i;
+            int j = 0;
+            for (i = 1; i > j; i++) {
+                System.out.println("Press 1 to continue adding contacts\nPress 2 to edit a contact\nPress 0 to exit the program");
+                Scanner in = new Scanner(System.in);
+                int operation = in.nextInt();
+                if (operation == 1) {
+                    contact.setUpInfo();
+                    contact.displayListItems();
+                } else if (operation == 2) {
+                    contact.editContact();
+                } else if (operation == 0) {
+                    j = i + 2;
+                }
+            }
         }
     }
+
